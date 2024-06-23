9fcf75dacce78cbaf234e29f772bd07cc088b6b9
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-06-23 14:49:11 -0400

    Cree el contexto y agrege el modelo de Articulos a este

---
+++ ArticulosAPi/DAL/Contexto.cs
@@ -0,0 +1,9 @@
+﻿using ArticulosAPi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArticulosAPi.DAL;
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    public DbSet<Articulos> Articulos { get; set; }
+}
