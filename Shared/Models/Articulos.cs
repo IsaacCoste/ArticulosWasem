688e0149ea34217a4096154ff27c29333949cc92
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-06-23 23:52:15 -0400

    Cree un libreria compartida para agregar el modelo de articulos

---
+++ Shared/Models/Articulos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models;
+public class Articulos
+{
+    [Key]
+    public int ArticuloId { get; set; }
+    [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑ0-9\s]+$", ErrorMessage = "Solo se permiten letras")]
+    public string Descripcion { get; set; }
+    [Range(0.01, float.MaxValue, ErrorMessage = "El costo debe ser mayor a 0")]
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public decimal Costo { get; set; }
+    [Range(0.01, 100, ErrorMessage = "La ganancia debe ser mayor a 0")]
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public decimal Ganancia { get; set; }
+    [Range(0.01, float.MaxValue, ErrorMessage = "El precio debe ser mayor a 0")]
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public decimal Precio { get; set; }
+}
